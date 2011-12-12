e2ef31a8b21ef149f726dec15dcaee0fc7c589aa
Valentyn Kukushkin <valentyn.kukushkin@gmail.com>
2011-12-12 23:18:15 +0100

    initial commit

---
+++ TableView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by Valentyn Kukushkin on 12/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
