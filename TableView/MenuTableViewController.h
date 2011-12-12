746fdfa5320ae541abb6b7efd86ee85d5e264289
Valentyn Kukushkin <valentyn.kukushkin@gmail.com>
2011-12-12 23:24:04 +0100

    1.a - added MenuTableViewController

---
+++ TableView/MenuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuTableViewController.h
+//  TableView
+//
+//  Created by Valentyn Kukushkin on 12/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuTableViewController : UIViewController
+
+@end
